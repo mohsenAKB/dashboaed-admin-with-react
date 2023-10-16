94bb639e59c84b1972b96c4f5323d2f3ef86059f
unknown <mohsen.akb1377@gmail.com>
2023-10-16 18:02:37 +0330

    Modify routes.js :import NewUsers component

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Home from "./pages/home/Home";
 import UsersList from "./pages/users/UsersList";
+import NewUsers from "./pages/newUsers/NewUsers";
 
 
 let routes = [
   { path: "/", element: <Home /> },
   { path: "/users", element: <UsersList /> },
+  { path: "/newUsers", element: <NewUsers /> },
 ];
 
 
