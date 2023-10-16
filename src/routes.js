502ca13492a91e00070fae6b696b34d731b6faa1
unknown <mohsen.akb1377@gmail.com>
2023-10-16 16:10:44 +0330

    Modify routes.js :import UsersList component & add route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Home from "./pages/home/Home";
+import UsersList from "./pages/users/UsersList";
 
 
-let routes =[
-    {path : '/', element: <Home />}
-]
+let routes = [
+  { path: "/", element: <Home /> },
+  { path: "/users", element: <UsersList /> },
+];
 
 
 export default routes
