5f269226f53a9ca306a4a17f4600d464c3b72ac0
unknown <mohsen.akb1377@gmail.com>
2023-10-16 18:07:58 +0330

    Modify routes.js :import Product  component

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import Home from "./pages/home/Home";
 import UsersList from "./pages/users/UsersList";
 import NewUsers from "./pages/newUsers/NewUsers";
+import NewUsers from "./pages/products/Products";
 
 
 let routes = [
   { path: "/", element: <Home /> },
   { path: "/users", element: <UsersList /> },
   { path: "/newUsers", element: <NewUsers /> },
+  { path: "/products", element: <Products /> },
 ];
 
 
