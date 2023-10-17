ff086075dd49f054f96eb6c2b3ff7a77a77b2c04
unknown <mohsen.akb1377@gmail.com>
2023-10-17 16:36:53 +0330

    Modify TopBar component:add Style and JSX

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
 import "./TopBar.css";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
 const TopBar = () => {
-    return (
-        <div>
-            topbar
+  return (
+    <section className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">SabzLearn</span>
         </div>
-    );
-}
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <LanguageIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <img src="img/mohsen.jpg" className="topAvatar" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default TopBar;
