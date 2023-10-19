d8020964e128e925be703d462650f234332e11fd
unknown <mohsen.akb1377@gmail.com>
2023-10-19 17:03:30 +0330

    Modify Home component:import chart component and use it

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Home.css';
 import Feature from "../../components/features/Feature";
+import Chart from "../../components/chart/Chart";
+import {xAxisData} from "../../datas";
 const Home = () => {
     return (
-        <>
-            <Feature />
-        </>
+      <section className="home">
+        <Feature />
+        <Chart grid title="Month sale" data={xAxisData} datakey="Sale"/>
+      </section>
     );
 }
 
